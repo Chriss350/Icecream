d4972d8eed7b98cfe2711c39b4121b8f979b7db5
Michal Raszczyk <mraszczyk@gmail.com>
2022-09-07 19:54:55 +0200

    feature / order modal
    modal działa - do poprawy temat wysokości na horyzontalnym ekranie mobilnym

--- src/scripts/order-form-modal/order-modal.js
+++ src/scripts/order-form-modal/order-modal.js
@@ -2,7 +2,26 @@
     const refs = {
         openModalBtn: document.querySelector("[data-order-modal-open]"),
         closeModalBtn: document.querySelector("[data-order-modal-close]"),
-        modal: document.querySelector("[data-modal]"),
+        modal: document.querySelector("[data-order-modal]"),
+    };
+
+    refs.openModalBtn.addEventListener("click", SlideIn);
+    refs.closeModalBtn.addEventListener("click", SlideOut);
+
+    function SlideIn() {
+        refs.modal.className = "modal__container in";
+    }
+
+    function SlideOut() {
+        refs.modal.classList.add("out");
+    }
+})();
+
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-order-modal-open2]"),
+        closeModalBtn: document.querySelector("[data-order-modal-close]"),
+        modal: document.querySelector("[data-order-modal]"),
     };
 
     refs.openModalBtn.addEventListener("click", SlideIn);
