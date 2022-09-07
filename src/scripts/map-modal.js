fd57d6046fb0d5cee62a6d7a993f134666c552eb
Chriss350 <106681377+Chriss350@users.noreply.github.com>
2022-09-07 22:17:32 +0200

    franchise modal

--- src/scripts/map-modal.js
+++ src/scripts/map-modal.js
@@ -3,10 +3,15 @@
     openMapModalBtn: document.querySelector('[data-map-open]'),
     closeMapModalBtn: document.querySelector('[data-map-close]'),
     modalMap: document.querySelector('[data-map-modal]'),
+    openFranchiseModalBtn: document.querySelector('[data-franchise-open]'),
+    closeFranchiseModalBtn: document.querySelector('[data-franchise-modal-close]'),
+    modalFranchise: document.querySelector('[data-franchise-modal]'),
   };
 
   refs.openMapModalBtn.addEventListener('click', MapSlideIn);
   refs.closeMapModalBtn.addEventListener('click', MapSlideOut);
+  refs.openFranchiseModalBtn.addEventListener('click', FranchiseSlideIn);
+  refs.closeFranchiseModalBtn.addEventListener('click', FranchiseSlideOut);
 
   function MapSlideIn() {
     refs.modalMap.className = 'modal__container in';
@@ -14,5 +19,13 @@
 
   function MapSlideOut() {
     refs.modalMap.classList.add('out');
+  }
+
+  function FranchiseSlideIn() {
+    refs.modalFranchise.className = 'modal__container in';
+  }
+
+  function FranchiseSlideOut() {
+    refs.modalFranchise.classList.add('out');
   }
 })();
