0b272f5b7e18d2de00a8c69fc1142980455a6a6f
Chriss350 <106681377+Chriss350@users.noreply.github.com>
2022-09-08 20:01:33 +0200

    modal products

--- src/scripts/map-modal.js
+++ src/scripts/map-modal.js
@@ -6,12 +6,34 @@
     openFranchiseModalBtn: document.querySelector('[data-franchise-open]'),
     closeFranchiseModalBtn: document.querySelector('[data-franchise-modal-close]'),
     modalFranchise: document.querySelector('[data-franchise-modal]'),
+    icereamBtnOpen: document.querySelector('[data-icecream-modal-open]'),
+    iceream1BtnOpen: document.querySelector('[data-icecream1-modal-open]'),
+    iceream2BtnOpen: document.querySelector('[data-icecream2-modal-open]'),
+    icereamBtnClose: document.querySelector('[data-icecream-modal-close]'),
+    modalIcecream: document.querySelector('[data-icecream-modal]'),
   };
 
+  //Map
   refs.openMapModalBtn.addEventListener('click', MapSlideIn);
   refs.closeMapModalBtn.addEventListener('click', MapSlideOut);
+  //Franchise
   refs.openFranchiseModalBtn.addEventListener('click', FranchiseSlideIn);
   refs.closeFranchiseModalBtn.addEventListener('click', FranchiseSlideOut);
+  //icecream
+  refs.icereamBtnOpen.addEventListener('click', IcecreamSlideIn);
+  refs.iceream1BtnOpen.addEventListener('click', IcecreamSlideIn);
+  refs.iceream2BtnOpen.addEventListener('click', IcecreamSlideIn);
+  refs.icereamBtnClose.addEventListener('click', IcecreamSlideOut);
+
+  function IcecreamSlideIn() {
+    refs.modalIcecream.className = 'modal__container in';
+    document.body.style.overflow = 'hidden';
+  }
+
+  function IcecreamSlideOut() {
+    document.body.style.overflow = 'auto';
+    refs.modalIcecream.classList.add('out');
+  }
 
   function MapSlideIn() {
     refs.modalMap.className = 'modal__container in';
